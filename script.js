74b5a1b13e1a31352753f7cf99b31368eeadbe01
szympie629 <szymon.piela.98@gmail.com>
2026-02-03 19:51:48 +0100

    konfiguracja bazy danych

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// Dane do połączenia z Twoim projektem Supabase
+const supabaseUrl = 'https://zmwnvciqxphttbcunxa.supabase.co';
+const supabaseKey = 'sb_publishable_3F-Hr9mWKyPrwiD7-fzw0A_r3SeGW2v';
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+const regForm = document.getElementById('registration-form');
+const message = document.getElementById('message');
+
+// Obsługa wysyłania formularza
+regForm.addEventListener('submit', async (e) => {
+    e.preventDefault(); // Powstrzymujemy odświeżenie strony
+
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+    // Wysyłamy dane do tabeli 'users'
+    const { data, error } = await _supabase
+        .from('users')
+        .insert([{ username: user, password: pass }]);
+
+    if (error) {
+        message.innerText = "Rejestracja nie powiodła się: " + error.message;
+        message.style.color = "red";
+        // To jest giga extra komnentarz
+    } else {
+        message.innerText = "Rejestracja zakończona pomyślnie!";
+        message.style.color = "green";
+        regForm.reset(); // Czyścimy pola formularza
+    }
+});
+
 // Znajdujemy przycisk i kontener formularza w dokumencie
 const showFormBtn = document.getElementById('show-form-btn');
 const regContainer = document.getElementById('registration-container');
